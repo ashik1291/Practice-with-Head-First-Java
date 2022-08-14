a90ca7b583395dfc5b734fc53f9dac9d637cdf60
Md. Ashikur Rahman Bhuiyan <ashik1291@users.noreply.github.com>
2022-08-14 12:13:36 +0600

    Creating a Dog class and it's object in Test Drive

---
+++ DogTestDrive.java
@@ -0,0 +1,9 @@
+package com.headfirstJava;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.size = 40;
+        dog.bark();
+    }
+}
