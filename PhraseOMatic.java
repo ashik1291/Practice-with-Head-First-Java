0156c2f1a46b48b17e22a4ad6fb60db534b63177
Md. Ashikur Rahman Bhuiyan <ashik1291@users.noreply.github.com>
2022-08-14 12:39:02 +0600

    Random Phrase Generator with test drive

---
+++ PhraseOMatic.java
@@ -0,0 +1,33 @@
+package com.headfirstJava;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {"agnostic", "opinionated",
+                "voice activated", "haptically driven", "extensible",
+                "reactive", "agent based", "functional", "AI enabled",
+                "strongly typed"};
+        String[] wordListTwo = {"loosely coupled", "six sigma",
+                "asynchronous", "event driven", "pub-sub", "IoT", "cloud native", "service oriented", "containerized", "serverless",
+                "microservices", "distributed ledger"};
+        String[] wordListThree = {"framework", "library",
+                "DSL", "REST API", "repository", "pipeline", "service mesh", "architecture", "perspective", "design",
+                "orientation"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        Random random = new Random();
+        int rand1 = random.nextInt(oneLength);
+        int rand2 = random.nextInt(twoLength);
+        int rand3 = random.nextInt(threeLength);
+
+        String phrase = wordListOne[rand1] + " " +
+                wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        System.out.println("what we need is: " + phrase);
+    }
+}
